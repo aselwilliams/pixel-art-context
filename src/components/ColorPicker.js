a0cd94fd59aed96987dec64b6ceb42dbbce77ae4
aselwilliams <aselwilliams22@gmail.com>
2022-10-18 22:03:40 -0500

    formatted

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,15 +1,29 @@
-import {ColorContext} from '../ColorContext';
-import {useContext} from 'react';
+import { ColorContext } from "../ColorContext";
+import { useContext } from "react";
 
 export default function ColorPicker() {
-    const colors = ["red", "blue", "yellow", "green", "black", "white", "purple"];
-    const {setColor} = useContext(ColorContext);
+  const colors = [
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "green",
+    "brown",
+    "black",
+    "white",
+    "purple",
+  ];
+  const { setColor } = useContext(ColorContext);
   return (
     <div>
-        <h1>Choose a color</h1>
-        {colors.map((color) => (
-            <button key={color} onClick={()=>setColor(color)} style={{backgroundColor: color}}/>
-        ))}
+      <h1>Choose a color</h1>
+      {colors.map((color) => (
+        <button
+          key={color}
+          onClick={() => setColor(color)}
+          style={{ backgroundColor: color }}
+        />
+      ))}
     </div>
-  )
+  );
 }
--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -1,10 +1,18 @@
-import {useContext, useState} from 'react';
-import {ColorContext} from '../ColorContext'
+import { useContext, useState } from "react";
+import { ColorContext } from "../ColorContext";
 
 export default function Pixel() {
-    const {color} = useContext(ColorContext);
-    const [pixelColor, setPixelColor] = useState('lightGrey');
-    return (
-        <div onClick={()=> setPixelColor(color)} style={{height:'1.5rem', width:'1.5rem', backgroundColor: pixelColor, margin: '1px'}} />
-    )
+  const { color } = useContext(ColorContext);
+  const [pixelColor, setPixelColor] = useState("lightGrey");
+  return (
+    <div
+      onClick={() => setPixelColor(color)}
+      style={{
+        height: "1.5rem",
+        width: "1.5rem",
+        backgroundColor: pixelColor,
+        margin: "1px",
+      }}
+    />
+  );
 }
