4e45d56c3cb213c55ee23c2ca21c844436f34402
aselwilliams <aselwilliams22@gmail.com>
2022-10-18 20:51:22 -0500

    initial commit

---
+++ src/components/PixelArt.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ColorPicker from "./ColorPicker";
+import Pixels from "./Pixels";
+
+export default function PixelArt() {
+  return (
+    <div>
+      <ColorPicker />
+      <Pixels />
+    </div>
+  );
+}
